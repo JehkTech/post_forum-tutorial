d579bab461fd0caa8b2ea0af38fe140cafc09cbc
jeh <jehoiachinkatemangwe1@gmail.com>
2024-11-01 15:38:27 +0200

    Created API route from phoenix router endpoint to browser render. -rendered json output -successful post request test pass in postman -created mix phx.gen.json Posts Post posts body:string title:string

--- lib/forum_web/controllers/page_controller.ex
+++ lib/forum_web/controllers/page_controller.ex
@@ -6,4 +6,13 @@ defmodule ForumWeb.PageController do
     # so skip the default app layout.
     render(conn, :home, layout: false)
   end
+  def users(conn, _params) do
+    IO.puts("Users fi=unction hit!")
+    users = [
+      %{id: 1, name: "Alice", email: "alice@email.com"},
+      %{id: 2, name: "Mark", email: "mark@email.com"}
+    ]
+    # render(conn, :users, users: users, layout: false)
+    json(conn, %{users: users})
+  end
 end
